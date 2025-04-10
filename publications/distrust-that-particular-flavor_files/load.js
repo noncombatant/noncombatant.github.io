61236f5e4e335376ba03562109b8e796a578278c
Chris Palmer <chris@noncombatant.org>
2025-04-09 22:14:58 -0700

    update

--- publications/distrust-that-particular-flavor_files/load.js
+++ publications/distrust-that-particular-flavor_files/load.js
@@ -1,1 +1,84 @@
-var d,pm_ppy="gawkermedia-network",_pmpmk,_pmasync,_pmoptimization,_pmoptimizationmanipulation,_pmhp,_pmsb,bbVer,pmk,pmglb,pmfa,pmad,pmdebug_c,_pmenv,rand,_pm_mcm,_pm_mcg,_pm_mur,_pm_spcm,pm_geo="US",_pmep="//widget.perfectmarket.com/",_pmep_geo="//geo.perfectmarket.com/";document.URL.indexOf("https://")>-1&&(_pmep=_pmep.replace(/88\//gi,"90/"),_pmep_geo=_pmep_geo.replace(/88\//gi,"90/")),_pmpmk="gawkermedia-network/pmk-201543005.4.js",_pmasync=!0,_pmoptimization=!0,_pmoptimizationmanipulation=!0,_pmhp=!1,pm_ppy=="msn-network"&&pm_pgtp=="home"&&_pmhp===!0&&(_pmpmk="gawkermedia-network/pmk-hp-201543005.4.js"),_pmsb=!1;function _pmloadfile(t){var n=document.getElementsByTagName("head")[0],e=document.createElement("script");e.setAttribute("type","text/javascript"),e.setAttribute("src",t),n.appendChild(e)}if(bbVer=getBBVersion(),(bbVer==null||parseInt(bbVer)>5)&&(pm_ppy=="msn-network"&&(pm_pgtp!="home"||_pmhp)||pm_ppy!="msn-network")){pmglb=pmglb||null,pmfa=pmfa||null,pmad=pmad||null,pmdebug_c=pmdebug_c||null,pmk=pmk||null,_pmenv=getUrlParameter("pmenv"),_pmenv&&_pmenv=="sandbox"&&!_pmsb&&(_pmep=_pmep.replace("//widget.perfectmarket.com","//widget.sandbox.perfectmarket.com"),_pmep_geo=_pmep_geo.replace("//geo.perfectmarket.com","//geo.sandbox.perfectmarket.com"),d=new Date,rand=d.getTime(),_pmpmk=pm_ppy+"/load.js?"+rand),function(){var e="script",t=document;_pmloadfile(_pmep+_pmpmk)}();function pmws_request_done(){var t,n="script",e=document;e.all&&!window.opera&&(e.write("<"+n+' type="text/javascript" id="pm_contentloadtag" defer="defer" src="javascript:void(0)"></'+n+">"),t=e.getElementById("pm_contentloadtag"),t&&(t.onreadystatechange=function(){if(this.readyState=="complete")return})),_pmloadfile(_pmep+_pmpmk)}}function getBBVersion(){var n,s,e=navigator.userAgent,t=null;return e.indexOf("BlackBerry")>=0&&(e.indexOf("Version/")>=0?(n=e.indexOf("Version/")+8,t=e.substring(n,n+3)):(s=e.split("/"),t=s[1].substring(0,3))),t}function getUrlParameter(t){var e=RegExp("[?&]"+t+"=([^&]*)").exec(window.location.search);return e&&decodeURIComponent(e[1].replace(/\+/g," "))}_pm_mcm={ts:1445466819,modules:{gwkcmt:{v:"gwktest",d:{k:["SITE"],fbk:!0},b:{"thevane.gawker":.52281,gawker:.1234567,"jalopnik.gawker":.9999,gizmodo:.777,io9:.123,_def_:0},u:{h:.202333,m:.1214,vt:"vis",en:!1}}}},_pm_mcg={me:["facebook","twitter","gawker","gwkcmt"],rs:"interactive",modules:{gwkcmt:{d:{hP:"N",hN:"N",mP:"N",mN:"N",lP:"N",lN:"N"},pal:{m:{l:"//div[@id='taboola-right-rail-thumbnails']",t:"before",c:"aside .nextcontentitem:last + *"}},sa:{s:"steep",r:"25%"},dm:{s:"N",m:"N",l:"N"},c:{h:"0.33",m:"0.67"},s:1,a:0}},pe:!0},_pm_mur={modules:{gwkcmt:{mix:{v:{t:"LG5",p:{A:-.25,K:.95,B:.01,v:.4,Q:.875}},a:{t:"LG5",p:{A:-.25,K:.95,B:.1,v:.4,Q:.90365}},o:{t:"LG5",p:{A:-.02,K:1,B:5,v:.4,Q:3.90365}}},arm:{t:12,m:{0:{t:"SS4",p:{a:.060254,b:1.03272,c:13.8905,d:0}},1:{t:"SS4",p:{a:.149504,b:1.02716,c:11.6466,d:0}},2:{t:"SS4",p:{a:.242157,b:1.00198,c:10.4334,d:0}},3:{t:"SS4",p:{a:.377447,b:.96697,c:8.02161,d:0}},4:{t:"SS4",p:{a:.719076,b:.875771,c:3.91168,d:0}},5:{t:"SS4",p:{a:2.58095,b:.788943,c:.672927,d:0}},6:{t:"SS4",p:{a:179285,b:.7807,c:.470469e-6,d:0}},7:{t:"SS4",p:{a:188718,b:.813134,c:.108058e-5,d:0}},8:{t:"SS4",p:{a:914848,b:.837949,c:.327204e-6,d:0}},9:{t:"SS4",p:{a:1433430,b:.907573,c:.9291e-6,d:0}},10:{t:"SS4",p:{a:458067,b:.918947,c:.450066e-5,d:0}},11:{t:"SS4",p:{a:997877,b:.926277,c:.240021e-5,d:0}},12:{t:"SS4",p:{a:779498,b:.870516,c:.123848e-5,d:0}}}},rk:[{r:0,c:1}]}}},_pm_spcm={xscale:100,shape:{flat:{"5%":{t:"LG3",p:{B:.012,M:35.225,v:.1883}},"10%":{t:"LG3",p:{B:.012,M:25.225,v:.2283}},"15%":{t:"LG3",p:{B:.012,M:12.115,v:.2483}},"20%":{t:"LG3",p:{B:.012,M:-1.115,v:.26032}},"25%":{t:"LG3",p:{B:.012,M:-8.115,v:.28464}},"30%":{t:"LG3",p:{B:.012,M:-18.115,v:.29891}},"35%":{t:"LG3",p:{B:.012,M:-23.115,v:.32812}},"40%":{t:"LG3",p:{B:.012,M:-30.972,v:.34912}},"45%":{t:"LG3",p:{B:.012,M:-40.972,v:.36338}},"50%":{t:"LG3",p:{B:.012,M:-45.972,v:.39935}},"55%":{t:"LG3",p:{B:.012,M:-50.972,v:.44139}},"60%":{t:"LG3",p:{B:.012,M:-58.972,v:.4768}},"65%":{t:"LG3",p:{B:.012,M:-66.972,v:.52131}},"70%":{t:"LG3",p:{B:.012,M:-75.972,v:.57375}},"75%":{t:"LG3",p:{B:.012,M:-85.972,v:.64049}},"80%":{t:"LG3",p:{B:.012,M:-90.972,v:.78444}},"85%":{t:"LG3",p:{B:.013,M:-95.972,v:.90026}},"90%":{t:"LG3",p:{B:.01523,M:-99.972,v:.98989}},"95%":{t:"LG3",p:{B:.02074,M:-99.972,v:.9907}}},mod:{"5%":{t:"LG3",p:{B:.09,M:77.749,v:.19}},"10%":{t:"LG3",p:{B:.09,M:70.749,v:.20651}},"15%":{t:"LG3",p:{B:.085,M:63.749,v:.2141}},"20%":{t:"LG3",p:{B:.085,M:58.1,v:.21816}},"25%":{t:"LG3",p:{B:.082,M:51.899,v:.21943}},"30%":{t:"LG3",p:{B:.0795,M:45.899,v:.22016}},"35%":{t:"LG3",p:{B:.0775,M:40.052,v:.22016}},"40%":{t:"LG3",p:{B:.0745,M:34.052,v:.22226}},"45%":{t:"LG3",p:{B:.0725,M:28.223,v:.22226}},"50%":{t:"LG3",p:{B:.0715,M:26.553,v:.28219}},"55%":{t:"LG3",p:{B:.0715,M:21.553,v:.28368}},"60%":{t:"LG3",p:{B:.0725,M:17.553,v:.29743}},"65%":{t:"LG3",p:{B:.0735,M:13.553,v:.31252}},"70%":{t:"LG3",p:{B:.0745,M:9.553,v:.33007}},"75%":{t:"LG3",p:{B:.0749,M:4.553,v:.33376}},"80%":{t:"LG3",p:{B:.0759,M:-.553,v:.33753}},"85%":{t:"LG3",p:{B:.0789,M:-4.553,v:.36707}},"90%":{t:"LG3",p:{B:.0819,M:-10.553,v:.37578}},"95%":{t:"LG3",p:{B:.1279,M:-5.753,v:.57175}}},steep:{"5%":{t:"LG3",p:{B:.2779,M:92.819,v:.57175}},"10%":{t:"LG3",p:{B:.2799,M:87.396,v:.59181}},"15%":{t:"LG3",p:{B:.2799,M:82.264,v:.59181}},"20%":{t:"LG3",p:{B:.2799,M:77.233,v:.59188}},"25%":{t:"LG3",p:{B:.2799,M:72.226,v:.59196}},"30%":{t:"LG3",p:{B:.2799,M:67.224,v:.59196}},"35%":{t:"LG3",p:{B:.2799,M:62.224,v:.59202}},"40%":{t:"LG3",p:{B:.2799,M:57.224,v:.59203}},"45%":{t:"LG3",p:{B:.2799,M:52.224,v:.59204}},"50%":{t:"LG3",p:{B:.2799,M:47.224,v:.59204}},"55%":{t:"LG3",p:{B:.2799,M:42.224,v:.59204}},"60%":{t:"LG3",p:{B:.2799,M:37.224,v:.59204}},"65%":{t:"LG3",p:{B:.2799,M:32.224,v:.59204}},"70%":{t:"LG3",p:{B:.2799,M:27.224,v:.59204}},"75%":{t:"LG3",p:{B:.2799,M:22.224,v:.59204}},"80%":{t:"LG3",p:{B:.2799,M:17.224,v:.59211}},"85%":{t:"LG3",p:{B:.2799,M:12.219,v:.59221}},"90%":{t:"LG3",p:{B:.2799,M:7.1628,v:.59221}},"95%":{t:"LG3",p:{B:.2862,M:1.7569,v:.59224}}}}}
+var pm_ppy="gawkermedia-network";var pm_geo="US";
+
+var _pmep = '//widget.perfectmarket.com/'; var _pmep_geo = '//geo.perfectmarket.com/'; if (document.URL.indexOf('https://') > -1) { _pmep = _pmep.replace(/88\//gi, '90/'); _pmep_geo = _pmep_geo.replace(/88\//gi, '90/'); } var _pmpmk = 'gawkermedia-network/pmk-201543005.4.js'; var _pmasync = true; var _pmoptimization = true; var _pmoptimizationmanipulation = true; var _pmhp = false;
+if( pm_ppy == "msn-network" && pm_pgtp == "home" && (_pmhp === true))  _pmpmk = 'gawkermedia-network/pmk-hp-201543005.4.js';
+var _pmsb = false;function _pmloadfile(fileName) {
+        
+          var js,elements = document.getElementsByTagName("head")[0]; js = document.createElement("script"); js.setAttribute("type","text/javascript"); js.setAttribute("src", fileName); elements.appendChild(js);
+}
+var bbVer = getBBVersion(); if ((bbVer == null || parseInt(bbVer) > 5) && ((pm_ppy=="msn-network" && (pm_pgtp!="home"|| _pmhp)) || pm_ppy!="msn-network")) {
+var pmk,pmglb,pmfa,pmad,pmdebug_c;pmglb=pmglb||null;pmfa=pmfa||null;pmad=pmad||null;pmdebug_c=pmdebug_c||null;pmk=pmk||null; var _pmenv = getUrlParameter('pmenv');
+if(_pmenv && _pmenv == 'sandbox' && !_pmsb) {_pmep=_pmep.replace('//widget.perfectmarket.com', '//widget.sandbox.perfectmarket.com');_pmep_geo=_pmep_geo.replace('//geo.perfectmarket.com', '//geo.sandbox.perfectmarket.com');var d = new Date();var rand = d.getTime();_pmpmk=pm_ppy+"/load.js?"+rand;}
+(function(){ var sc='script',doc=document; _pmloadfile(_pmep+_pmpmk); })();
+function pmws_request_done(){ var sc="script",doc=document; if (doc.all && !window.opera){doc.write('<'+sc+' type="text/javascript" id="pm_contentloadtag" defer="defer" src="javascript:void(0)"><\/'+sc+'>');var pm_contentloadtag = doc.getElementById("pm_contentloadtag");if(pm_contentloadtag)pm_contentloadtag.onreadystatechange = function() { if (this.readyState=="complete") return; } } _pmloadfile(_pmep+_pmpmk); }
+}
+function getBBVersion() { var ua = navigator.userAgent,ver=null,vp,splitUA; if (ua.indexOf("BlackBerry") >= 0) {if (ua.indexOf("Version/") >= 0) {vp = ua.indexOf("Version/") + 8;ver = ua.substring(vp, vp + 3);}else {splitUA = ua.split("/"); ver = splitUA[1].substring(0, 3);}} return ver; }
+function getUrlParameter(name) {var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);return match && decodeURIComponent(match[1].replace(/\+/g, ' '));}
+
+
+ /** Generated CJS **/ var _pm_mcm = {"ts":1445466819,"modules":{
+
+"gwkcmt":{"v":"gwktest","d":{"k":["SITE"],"fbk":true},"b":{"thevane.gawker":0.52281, "gawker":0.1234567, "jalopnik.gawker":0.9999, "gizmodo":0.777, "io9":0.123, "_def_":0},"u":{"h":0.202333,"m":0.1214,"vt":"vis", "en":false}}}};
+
+var _pm_mcg = {
+'me': ['facebook','twitter', 'gawker', 'gwkcmt'],
+'rs': 'interactive',
+    "modules":{
+ "gwkcmt":{
+            "d": {
+                "hP":"N",
+                "hN":"N",
+                "mP":"N",
+                "mN":"N",
+                "lP":"N",
+                "lN":"N"
+            },
+            "pal": {
+                "m": {
+                    "l":"//div[@id='taboola-right-rail-thumbnails']",
+                    "t":"before",
+"c":'aside .nextcontentitem:last + *'
+                }
+            },
+            "sa": {"s": "steep", "r":"25%"},
+            "dm": {"s":"N","m":"N","l":"N"},
+            "c": {
+                "h":"0.33",
+                "m":"0.67"
+            },
+            "s":1.0,
+            "a":0.00
+        }
+
+    },
+
+    "pe": true
+};
+var _pm_mur = {"modules":{
+"gwkcmt":{ 
+"mix":{
+"v":{"t":"LG5","p":{"A":-0.25,"K":0.95,"B":0.01,"v":0.4,"Q":0.875}},
+"a":{"t":"LG5","p":{"A":-0.25,"K":0.95,"B":0.1,"v":0.4,"Q":0.90365}},
+"o":{"t":"LG5","p":{"A":-0.02,"K":1,"B":5,"v":0.4,"Q":3.90365}}
+},
+"arm":{
+"t":12,
+"m":{
+"0":{"t":"SS4","p":{"a":6.02540E-02,"b":1.03272E+00,"c":1.38905E+01,"d":0}},
+"1":{"t":"SS4","p":{"a":1.49504E-01,"b":1.02716E+00,"c":1.16466E+01,"d":0}},
+"2":{"t":"SS4","p":{"a":2.42157E-01,"b":1.00198E+00,"c":1.04334E+01,"d":0}},
+"3":{"t":"SS4","p":{"a":3.77447E-01,"b":9.66970E-01,"c":8.02161E+00,"d":0}},
+"4":{"t":"SS4","p":{"a":7.19076E-01,"b":8.75771E-01,"c":3.91168E+00,"d":0}},
+"5":{"t":"SS4","p":{"a":2.58095E+00,"b":7.88943E-01,"c":6.72927E-01,"d":0}},
+"6":{"t":"SS4","p":{"a":1.79285E+05,"b":7.80700E-01,"c":4.70469E-07,"d":0}},
+"7":{"t":"SS4","p":{"a":1.88718E+05,"b":8.13134E-01,"c":1.08058E-06,"d":0}},
+"8":{"t":"SS4","p":{"a":9.14848E+05,"b":8.37949E-01,"c":3.27204E-07,"d":0}},
+"9":{"t":"SS4","p":{"a":1.43343E+06,"b":9.07573E-01,"c":9.29100E-07,"d":0}},
+"10":{"t":"SS4","p":{"a":4.58067E+05,"b":9.18947E-01,"c":4.50066E-06,"d":0}},
+"11":{"t":"SS4","p":{"a":9.97877E+05,"b":9.26277E-01,"c":2.40021E-06,"d":0}},
+"12":{"t":"SS4","p":{"a":7.79498E+05,"b":8.70516E-01,"c":1.23848E-06,"d":0}}
+}
+},
+"rk":[{"r":0.0,"c":1.000000}]}}};
+var _pm_spcm = {"xscale":100,"shape":{"flat":{"5%":{"t":"LG3","p":{"B":0.012,"M":35.225,"v":0.1883}},"10%":{"t":"LG3","p":{"B":0.012,"M":25.225,"v":0.2283}},"15%":{"t":"LG3","p":{"B":0.012,"M":12.115,"v":0.2483}},"20%":{"t":"LG3","p":{"B":0.012,"M":-1.115,"v":0.26032}},"25%":{"t":"LG3","p":{"B":0.012,"M":-8.115,"v":0.28464}},"30%":{"t":"LG3","p":{"B":0.012,"M":-18.115,"v":0.29891}},"35%":{"t":"LG3","p":{"B":0.012,"M":-23.115,"v":0.32812}},"40%":{"t":"LG3","p":{"B":0.012,"M":-30.972,"v":0.34912}},"45%":{"t":"LG3","p":{"B":0.012,"M":-40.972,"v":0.36338}},"50%":{"t":"LG3","p":{"B":0.012,"M":-45.972,"v":0.39935}},"55%":{"t":"LG3","p":{"B":0.012,"M":-50.972,"v":0.44139}},"60%":{"t":"LG3","p":{"B":0.012,"M":-58.972,"v":0.47680}},"65%":{"t":"LG3","p":{"B":0.012,"M":-66.972,"v":0.52131}},"70%":{"t":"LG3","p":{"B":0.012,"M":-75.972,"v":0.57375}},"75%":{"t":"LG3","p":{"B":0.012,"M":-85.972,"v":0.64049}},"80%":{"t":"LG3","p":{"B":0.012,"M":-90.972,"v":0.78444}},"85%":{"t":"LG3","p":{"B":0.013,"M":-95.972,"v":0.90026}},"90%":{"t":"LG3","p":{"B":0.01523,"M":-99.972,"v":0.98989}},"95%":{"t":"LG3","p":{"B":0.02074,"M":-99.972,"v":0.99070}}},"mod":{"5%":{"t":"LG3","p":{"B":0.09,"M":77.749,"v":0.19}},"10%":{"t":"LG3","p":{"B":0.09,"M":70.749,"v":0.20651}},"15%":{"t":"LG3","p":{"B":0.085,"M":63.749,"v":0.21410}},"20%":{"t":"LG3","p":{"B":0.085,"M":58.100,"v":0.21816}},"25%":{"t":"LG3","p":{"B":0.082,"M":51.899,"v":0.21943}},"30%":{"t":"LG3","p":{"B":0.0795,"M":45.899,"v":0.22016}},"35%":{"t":"LG3","p":{"B":0.0775,"M":40.052,"v":0.22016}},"40%":{"t":"LG3","p":{"B":0.0745,"M":34.052,"v":0.22226}},"45%":{"t":"LG3","p":{"B":0.0725,"M":28.223,"v":0.22226}},"50%":{"t":"LG3","p":{"B":0.0715,"M":26.553,"v":0.28219}},"55%":{"t":"LG3","p":{"B":0.0715,"M":21.553,"v":0.28368}},"60%":{"t":"LG3","p":{"B":0.0725,"M":17.553,"v":0.29743}},"65%":{"t":"LG3","p":{"B":0.0735,"M":13.553,"v":0.31252}},"70%":{"t":"LG3","p":{"B":0.0745,"M":9.553,"v":0.33007}},"75%":{"t":"LG3","p":{"B":0.0749,"M":4.553,"v":0.33376}},"80%":{"t":"LG3","p":{"B":0.0759,"M":-0.553,"v":0.33753}},"85%":{"t":"LG3","p":{"B":0.0789,"M":-4.553,"v":0.36707}},"90%":{"t":"LG3","p":{"B":0.0819,"M":-10.553,"v":0.37578}},"95%":{"t":"LG3","p":{"B":0.1279,"M":-5.753,"v":0.57175}}},"steep":{"5%":{"t":"LG3","p":{"B":0.2779,"M":92.819,"v":0.57175}},"10%":{"t":"LG3","p":{"B":0.2799,"M":87.396,"v":0.59181}},"15%":{"t":"LG3","p":{"B":0.2799,"M":82.264,"v":0.59181}},"20%":{"t":"LG3","p":{"B":0.2799,"M":77.233,"v":0.59188}},"25%":{"t":"LG3","p":{"B":0.2799,"M":72.226,"v":0.59196}},"30%":{"t":"LG3","p":{"B":0.2799,"M":67.224,"v":0.59196}},"35%":{"t":"LG3","p":{"B":0.2799,"M":62.224,"v":0.59202}},"40%":{"t":"LG3","p":{"B":0.2799,"M":57.224,"v":0.59203}},"45%":{"t":"LG3","p":{"B":0.2799,"M":52.224,"v":0.59204}},"50%":{"t":"LG3","p":{"B":0.2799,"M":47.224,"v":0.59204}},"55%":{"t":"LG3","p":{"B":0.2799,"M":42.224,"v":0.59204}},"60%":{"t":"LG3","p":{"B":0.2799,"M":37.224,"v":0.59204}},"65%":{"t":"LG3","p":{"B":0.2799,"M":32.224,"v":0.59204}},"70%":{"t":"LG3","p":{"B":0.2799,"M":27.224,"v":0.59204}},"75%":{"t":"LG3","p":{"B":0.2799,"M":22.224,"v":0.59204}},"80%":{"t":"LG3","p":{"B":0.2799,"M":17.224,"v":0.59211}},"85%":{"t":"LG3","p":{"B":0.2799,"M":12.219,"v":0.59221}},"90%":{"t":"LG3","p":{"B":0.2799,"M":7.1628,"v":0.59221}},"95%":{"t":"LG3","p":{"B":0.2862,"M":1.7569,"v":0.59224}}}}};
+/** Generated CJS end **/ 
